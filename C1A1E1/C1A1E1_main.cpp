665f5485b512c2dfeb667bffe8d141ec31fe4d41
Shaun Chemplavil <schemp98@hotmail.com>
2020-04-01 15:45:18 -0500

    Answers are correct... need to confirm style

--- C1A1E1/C1A1E1_main.cpp
+++ C1A1E1/C1A1E1_main.cpp
@@ -7,7 +7,7 @@
 // C1A1E1_Main.cpp
 // Win10
 // Visual C++ 15.0
-// This file contains the most compact representation of expressions
+// This file contains the most appropriate form of expressions
 
 #include <iostream>
 using namespace std;
@@ -17,15 +17,15 @@ int main()
 
    cout << "\"ax = ax + bx\"  should be  \"ax += bx\"\n"
            "\"ax = ax / -bx\" should be  \"ax /= -bx\"\n"
-           "\"ax = bx / ax\"  should be  \"ax += bx\"\n" //
-           "\"ax = -1 * ax\"  should be  \"ax =  -ax\"\n"
+           "\"ax = bx / ax\"  should be  \"ax = bx / ax\"\n"  // Already in most appropriate form
+           "\"ax = -1 * ax\"  should be  \"ax = -ax\"\n"
            "\"ax = -ax * ax\" should be  \"ax *= -ax\"\n"
-           "\"ax = -bx * ax\" should be  \"ax += bx\"\n" ///
-           "\"ax = bx - ax\"  should be  \"ax = bx\"\n" ///
+           "\"ax = -bx * ax\" should be  \"ax *= -bx\"\n"
+           "\"ax = bx - ax\"  should be  \"ax = bx - ax\"\n"  // Already in most appropriate form
            "\"ax = 2 + ax\"   should be  \"ax += 2\"\n"
-           "\"ax = 1 + ax\"   should be  \"ax += 1\"\n"
+           "\"ax = 1 + ax\"   should be  \"ax++\"\n"          // Alternate form "++ax"
            "\"ax = ax - 37\"  should be  \"ax -= 37\"\n"
-           "\"ax = ax - 1\"   should be  \"ax -= 1\"\n"
+           "\"ax = ax - 1\"   should be  \"ax--\"\n"          // Alternate form "--ax"
            "\"ax *= -1\"      should be  \"ax = -ax\"\n"
            "\"ax /= -1\"      should be  \"ax = -ax\"\n"
            "\"ax = 0 - ax\"   should be  \"ax = -ax\"" << endl;
