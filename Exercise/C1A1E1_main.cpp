ca5ee8a4ecb24b78a1b9bc0bb65d62f09a81e589
Shaun Chemplavil <schemp98@hotmail.com>
2020-04-01 20:48:39 -0500

    Adding Course Information Made corrections suggested by "AutoChecker"... sigh

--- Exercise/C1A1E1_main.cpp
+++ Exercise/C1A1E1_main.cpp
@@ -4,29 +4,31 @@
 // C / C++ Programming I : Fundamental Programming Concepts
 // 146359 Raymond L.Mitchell, Jr., M.S.
 // 04 / 01 / 2020
-// C1A1E1_Main.cpp
+// C1A1E1_main.cpp
 // Win10
-// Visual C++ 15.0
+// Visual C++ 19.0
+//
 // This file contains the most appropriate form of expressions
+//
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-   cout << "\"ax = ax + bx\"  should be  \"ax += bx\"\n"
-           "\"ax = ax / -bx\" should be  \"ax /= -bx\"\n"
-           "\"ax = bx / ax\"  should be  \"ax = bx / ax\"\n"  // Already in most appropriate form
-           "\"ax = -1 * ax\"  should be  \"ax = -ax\"\n"
-           "\"ax = -ax * ax\" should be  \"ax *= -ax\"\n"
-           "\"ax = -bx * ax\" should be  \"ax *= -bx\"\n"
-           "\"ax = bx - ax\"  should be  \"ax = bx - ax\"\n"  // Already in most appropriate form
-           "\"ax = 2 + ax\"   should be  \"ax += 2\"\n"
-           "\"ax = 1 + ax\"   should be  \"ax++\"\n"          // Alternate form "++ax"
-           "\"ax = ax - 37\"  should be  \"ax -= 37\"\n"
-           "\"ax = ax - 1\"   should be  \"ax--\"\n"          // Alternate form "--ax"
-           "\"ax *= -1\"      should be  \"ax = -ax\"\n"
-           "\"ax /= -1\"      should be  \"ax = -ax\"\n"
-           "\"ax = 0 - ax\"   should be  \"ax = -ax\"" << endl;
+   cout <<  "\"ax = ax + bx\" should be \"ax += bx\"\n"
+            "\"ax = ax / -bx\" should be \"ax /= -bx\"\n"
+            "\"ax = bx / ax\" should be \"ax = bx / ax\"\n"  // no change
+            "\"ax = -1 * ax\" should be \"ax = -ax\"\n"
+            "\"ax = -ax * ax\" should be \"ax *= -ax\"\n"
+            "\"ax = -bx * ax\" should be \"ax *= -bx\"\n"
+            "\"ax = bx - ax\" should be \"ax = bx - ax\"\n"  // no change
+            "\"ax = 2 + ax\" should be \"ax += 2\"\n"
+            "\"ax = 1 + ax\" should be \"++ax\" or \"ax++\"\n"
+            "\"ax = ax - 37\" should be \"ax -= 37\"\n"
+            "\"ax = ax - 1\" should be \"--ax\" or \"ax--\"\n"
+            "\"ax *= -1\" should be \"ax = -ax\"\n"
+            "\"ax /= -1\" should be \"ax = -ax\"\n"
+            "\"ax = 0 - ax\" should be \"ax = -ax\"\n";
    return 0;
 }
--- Exercise/C1A1E2_main.c
+++ Exercise/C1A1E2_main.c
@@ -2,27 +2,31 @@
 // Shaun Chemplavil U08713628
 // schemp98@hotmail.com
 // C / C++ Programming I : Fundamental Programming Concepts
-// 146359 Raymond L.Mitchell, Jr., M.S.
+// 146359 Raymond L. Mitchell Jr.
 // 04 / 01 / 2020
-// C1A1E2_Main.c
+// C1A1E2_main.c
 // Win10
-// Visual C++ 15.0
+// Visual C++ 19.0
+//
 // This file will take user input X, and display the result of a function
+//
 
 #include <stdio.h>
 
 int main(void)
 {
    // Declare User Input (U), Result (R)
-   double U, R;
+   double u, r;
 
    // Request and Store User Input
-   printf("%s", "Please enter any decimal value: ");
-   scanf("%lf", &U);
+   printf("%s", "Enter a decimal value: ");
+   scanf("%lf", &u);
 
    // Calculate result (R)
-   R = 3.0 * U * U * U - 5.0 * U * U + 6.0;
+   r = 3.0 * u * u * u - 5.0 * u * u + 6.0;
 
-   printf("%s%g%s%g", "If x = ", U, " the value of \"3x^3 - 5x^2 + 6\" is ", R);
+   printf("%s%g%s%g%c", "If x = ", u,
+      " the value of \"3x^3 - 5x^2 + 6\" is ", r, '\n');
+
    return 0;
 }
