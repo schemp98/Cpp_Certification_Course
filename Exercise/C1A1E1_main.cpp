c51ee844551feaca4772cdddc00a15aa8184b781
Shaun Chemplavil <schemp98@hotmail.com>
2020-04-01 21:18:23 -0500

    Minor tweak to indentations to appease Assignment Checker

--- Exercise/C1A1E1_main.cpp
+++ Exercise/C1A1E1_main.cpp
@@ -16,19 +16,20 @@ using namespace std;
 
 int main()
 {
-   cout <<  "\"ax = ax + bx\" should be \"ax += bx\"\n"
-            "\"ax = ax / -bx\" should be \"ax /= -bx\"\n"
-            "\"ax = bx / ax\" should be \"ax = bx / ax\"\n"  // no change
-            "\"ax = -1 * ax\" should be \"ax = -ax\"\n"
-            "\"ax = -ax * ax\" should be \"ax *= -ax\"\n"
-            "\"ax = -bx * ax\" should be \"ax *= -bx\"\n"
-            "\"ax = bx - ax\" should be \"ax = bx - ax\"\n"  // no change
-            "\"ax = 2 + ax\" should be \"ax += 2\"\n"
-            "\"ax = 1 + ax\" should be \"++ax\" or \"ax++\"\n"
-            "\"ax = ax - 37\" should be \"ax -= 37\"\n"
-            "\"ax = ax - 1\" should be \"--ax\" or \"ax--\"\n"
-            "\"ax *= -1\" should be \"ax = -ax\"\n"
-            "\"ax /= -1\" should be \"ax = -ax\"\n"
-            "\"ax = 0 - ax\" should be \"ax = -ax\"\n";
+   cout <<
+      "\"ax = ax + bx\" should be \"ax += bx\"\n"
+      "\"ax = ax / -bx\" should be \"ax /= -bx\"\n"
+      "\"ax = bx / ax\" should be \"ax = bx / ax\"\n"  // no change
+      "\"ax = -1 * ax\" should be \"ax = -ax\"\n"
+      "\"ax = -ax * ax\" should be \"ax *= -ax\"\n"
+      "\"ax = -bx * ax\" should be \"ax *= -bx\"\n"
+      "\"ax = bx - ax\" should be \"ax = bx - ax\"\n"  // no change
+      "\"ax = 2 + ax\" should be \"ax += 2\"\n"
+      "\"ax = 1 + ax\" should be \"++ax\" or \"ax++\"\n"
+      "\"ax = ax - 37\" should be \"ax -= 37\"\n"
+      "\"ax = ax - 1\" should be \"--ax\" or \"ax--\"\n"
+      "\"ax *= -1\" should be \"ax = -ax\"\n"
+      "\"ax /= -1\" should be \"ax = -ax\"\n"
+      "\"ax = 0 - ax\" should be \"ax = -ax\"\n";
    return 0;
 }
