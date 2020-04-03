985c4a205e2c394170dfb7cc2dc2a0a07faa6c3f
Shaun Chemplavil <schemp98@hotmail.com>
2020-04-03 10:36:08 -0500

    Updated Quiz 1 with Date... Adding Quiz 2, and some practice code...

---
+++ Exercise/C1N1PE6_main.cpp
@@ -0,0 +1,56 @@
+//
+// Shaun Chemplavil U08713628
+// schemp98@hotmail.com
+// C / C++ Programming I : Fundamental Programming Concepts
+// 146359 Raymond L.Mitchell, Jr., M.S.
+// 04 / 01 / 2020
+// C1A1E1_main.cpp
+// Win10
+// Visual C++ 19.0
+//
+// This file contains the most appropriate form of expressions
+//
+
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+   int d, o, h;
+   //char o = d;
+   //char h = d;
+   // 1-6
+   //d = "16";
+   d = 7;
+   cout << "in decimal: " << d << " | in octal : " << oct << d
+      << " | in hex: " << hex << d << dec << endl;
+   d = 8;
+   cout << "in decimal: " << d << " | in octal : " << oct << d
+      << " | in hex: " << hex << d << dec << endl;
+   d = 15;
+   cout << "in decimal: " << d << " | in octal : " << oct << d
+      << " | in hex: " << hex << d << dec << endl;
+   d = 16;
+   cout << "in decimal: " << d << " | in octal : " << oct << d
+      << " | in hex: " << hex << d << dec << endl;
+   d = -1;
+   cout << "in decimal: " << d << " | in octal : " << oct << d
+      << " | in hex: " << hex << d << dec << endl;
+   d = 1024;
+   cout << "in decimal: " << d << " | in octal : " << oct << d
+      << " | in hex: " << hex << d << dec << endl;
+
+   d = -1024;
+   cout << "in decimal: " << d << " | in octal : " << oct << d
+      << " | in hex: " << hex << d << dec << endl;
+
+   cout << "\nThis is Problem 1-7\n";
+   cout << "Please enter the same number three times: ";
+   //cin >> oct;
+   cin >> d >> oct >> o >> hex >> h >> dec;
+
+   cout << d << " Oct: " << o << " Hex : " << h << endl;
+
+   return 0;
+}
