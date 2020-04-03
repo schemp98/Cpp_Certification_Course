ea6f32143fc3b35d30692aa4d173b901b6915e45
Shaun Chemplavil <schemp98@hotmail.com>
2020-04-03 15:00:17 -0500

    Added new homework C1A2E1

--- Exercise/C1A2E1_main.cpp
+++ Exercise/C1A2E1_main.cpp
@@ -9,21 +9,21 @@
 // Visual C++ 19.0
 //
 // This file converts a user-input character to lowercase
-// This is accomplished by adding "toLower" to the integer value of the input
-// ("toLower" = '32' is the difference between upper and lower case letters
+// This is accomplished by adding "TO_LOWER" to the integer value of the input
+// ("TO_LOWER" = '32' is the difference between upper and lower case letters
 //   Code entries in the ASCII table)
 // Off-nominal Case 1: If a non-Uppercase character is entered, the character
 //                     32 codes higher than the entered character will be output
 // Off-nominal Case 2: If a white-space character is entered, the character
-//                     32 codes higher than the entered character will be output
+//                     32 codes higher than the FIRST whitespace character
+//                     entered will be output
 //
 
 #include <iostream>
-#include<stdio.h>
 using namespace std;
 
 // Set variable to convert from upper to lowercase
-const int toLower = 'a' - 'A';
+const int TO_LOWER = 'a' - 'A';
 
 int main()
 {
@@ -34,10 +34,10 @@ int main()
    // Grab character from user
    cin.get(ch);
 
-   // Convert to lowercase (by adding ASCII offset) and output to screen
+   // Convert to lowercase (by adding ASCII Code offset) and output to screen
    cout
       << "\nThe lowercase equivalent of '" << ch << "' is '"
-      << char(ch + toLower) << "'\n";
+      << char(ch + TO_LOWER) << "'\n";
 
    return 0;
 }
